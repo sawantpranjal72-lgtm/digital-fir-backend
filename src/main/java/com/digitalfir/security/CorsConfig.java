72e8a772b922a876f2b1137da68e5f1914798147
pranjal <pranjalsawant@gmail.com>
2026-02-24 08:23:49 +0530

    changes saved

--- src/main/java/com/digitalfir/security/CorsConfig.java
+++ src/main/java/com/digitalfir/security/CorsConfig.java
@@ -17,21 +17,21 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-        		 "http://127.0.0.1:5500",
-        		    "http://localhost:5500",
-        		    "http://34.235.155.152",
-        		    "http://34.235.155.152:8080"
-        	 
-        	));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(List.of("*"));
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+            "http://34.235.155.152",
+            "http://34.235.155.152:8080"
+        ));
 
+        config.setAllowedMethods(List.of(
+            "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-       
 
         UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+            new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**", config);
         return source;
